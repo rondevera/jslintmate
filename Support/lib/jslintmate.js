87d07042c611ef2df46469aa913bce7008e68ced
Ron DeVera <ronald.devera@gmail.com>
2011-04-26 00:11:59 -0400

    Added keyboard shortcuts for up ('j'), down ('k'), and open ('enter')
    Removed 'esc' for closing window; fails if JSLintMate is triggered twice
    in a row, possibly because the second trigger opens a new JS session,
    and JS can only close its own window (or any window it opened). Besides,
    cmd-W works fine.

--- Support/lib/jslintmate.js
+++ Support/lib/jslintmate.js
@@ -2,6 +2,69 @@
 /*global TextMate */
 
 (function(d){
+  var Nav = {CUR: 'current'};
+
+  function $qs(selector) { return d.querySelector(selector);    }
+  function $qsa(selector){ return d.querySelectorAll(selector); }
+
+
+
+  /*** Nav ***/
+
+  Nav.getHighlighted = function(){
+    return $qs('ul.problems li.' + Nav.CUR);
+  };
+  Nav.openHighlighted = function(){
+    var curLink = $qs('ul.problems li.current a'),
+        ev;
+
+    if(!curLink){ return; }
+
+    ev = d.createEvent('HTMLEvents');
+    ev.initEvent('click', true,   // bubbling
+                          true);  // cancelable
+    curLink.dispatchEvent(ev);
+  };
+  Nav.highlightFirst = function(){
+    $qs('ul.problems li').className = Nav.CUR;
+  };
+  Nav.highlightPrev = function(){
+    var cur = Nav.getHighlighted(), prev,
+        items, i;
+
+    if(cur){
+      // CSS3 can't select a previous sibling, so do this the long way.
+      items = $qsa('ul.problems li:not(.alert)');
+      i     = items.length;
+      while(i--){
+        if(items[i-1] && items[i].className === Nav.CUR){
+          items[i-1].className = Nav.CUR;
+          items[i].className   = '';
+          break;
+        }
+      }
+    }else{
+      Nav.highlightFirst();
+    }
+  };
+  Nav.highlightNext = function(){
+    var cur = Nav.getHighlighted(), next;
+
+    if(cur){
+      next = $qs('ul.problems li.' + Nav.CUR + ' + li:not(.alert)');
+      if(next){
+        next.className = Nav.CUR;
+        cur.className  = '';
+      }
+    }else{
+      Nav.highlightFirst();
+    }
+  };
+
+
+
+  /*** Behaviors ***/
+
   // Handle link to bundle info
   (function(){
     var infoLink = d.querySelector('header a.info');
@@ -15,12 +78,17 @@
   }());
 
   // Set up keyboard shortcuts
-  d.addEventListener('keydown', function(ev){
-    switch(ev.which){
-      case 27: // escape
-        window.close();
-        ev.preventDefault();
-        break;
-    }
-  }, false);
+  if(d.querySelector('ul.problems')){
+    d.addEventListener('keydown', function(ev){
+      switch(ev.keyCode){
+        case 13: // enter
+          Nav.openHighlighted(); ev.preventDefault(); break;
+        case 74: // 'j'
+          Nav.highlightNext();   ev.preventDefault(); break;
+        case 75: // 'k'
+          Nav.highlightPrev();   ev.preventDefault(); break;
+      }
+    }, false);
+  }
+
 }(document));
