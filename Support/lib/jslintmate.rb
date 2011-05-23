67e4466218b952a60c7ffa037484e2eebaa4cacd
Ron DeVera <ronald.devera@gmail.com>
2011-05-22 23:39:50 -0400

    Moved `JSLintMate::Linter` into a separate file

--- Support/lib/jslintmate.rb
+++ Support/lib/jslintmate.rb
@@ -28,8 +28,10 @@
 #   curl -o jslint.js http://www.jslint.com/fulljslint.js
 #   curl -o jshint.js http://jshint.com/jshint.js
 
+$LOAD_PATH << File.join(ENV['TM_BUNDLE_SUPPORT'], 'lib')
 require 'cgi'
 require 'erb'
+require 'jslintmate/linter'
 
 module JSLintMate
   def self.version
@@ -105,70 +107,6 @@ module JSLintMate
       <a href="https://github.com/rondevera/jslintmate" class="info"
         title="More info on JSLintMate #{version}">info</a>
     }.strip.split.join(' ')
-  end
-
-  class Linter
-    DEFAULT_OPTIONS = {:undef => true}
-
-    attr_accessor :key, :name, :path, :options, :options_filepath
-
-    ### Class methods ###
-
-    def self.options_string_to_hash(options_string)
-      # Usage: 'a=1,b=2' # => {'a' => 1, 'b' => 2}
-      options_string.split(',').inject({}) do |hsh, kv|
-        k, v = kv.split('='); hsh.merge(k => v)
-      end
-    end
-
-    def self.options_hash_to_string(options_hash)
-      # Usage: {:a => 1, 'b' => 2} # => 'a=1,b=2'
-      options_hash.map { |k, v| "#{k}=#{v}" }.join(',')
-    end
-
-    def self.default_options
-      @default_options ||= options_hash_to_string(DEFAULT_OPTIONS)
-    end
-
-    ### Instance methods ###
-
-    def initialize(attrs)
-      if attrs[:key] && attrs[:key].to_sym == :jshint
-        self.key  = :jshint
-        self.name = 'JSHint'
-      else
-        self.key  = :jslint
-        self.name = 'JSLint'
-      end
-
-      self.path = attrs[:path] || JSLintMate.lib_path("#{key}.js")
-
-      [:options, :options_filepath].each do |attr_key|
-        self.send(:"#{attr_key}=", attrs[attr_key])
-      end
-
-      self.options =
-        options ? JSLintMate::Linter.options_string_to_hash(options) : {}
-    end
-
-    def to_s; name; end
-
-    def options_string
-      JSLintMate::Linter.options_hash_to_string(options)
-    end
-
-    def reverse_merge_options_from_file!
-      return unless options_filepath && File.readable?(options_filepath)
-
-      require 'yaml'
-
-      # Parse linter options file; file options take precedence over existing
-      # options (i.e., default/custom bundle options)
-      file_options =
-        YAML.load_file(options_filepath).reject{ |k, v| v.is_a?(Array) }
-      self.options = options.merge(file_options)
-    end
-
   end
 
 end # module JSLintMate
---
+++ Support/lib/jslintmate/linter.rb
@@ -0,0 +1,70 @@
+module JSLintMate
+  class Linter
+    DEFAULT_OPTIONS = {:undef => true}
+
+    attr_accessor :key, :name, :path, :options, :options_filepath
+
+
+
+    ### Class methods ###
+
+    def self.options_string_to_hash(options_string)
+      # Usage: 'a=1,b=2' # => {'a' => 1, 'b' => 2}
+      options_string.split(',').inject({}) do |hsh, kv|
+        k, v = kv.split('='); hsh.merge(k => v)
+      end
+    end
+
+    def self.options_hash_to_string(options_hash)
+      # Usage: {:a => 1, 'b' => 2} # => 'a=1,b=2'
+      options_hash.map { |k, v| "#{k}=#{v}" }.join(',')
+    end
+
+    def self.default_options
+      @default_options ||= options_hash_to_string(DEFAULT_OPTIONS)
+    end
+
+
+
+    ### Instance methods ###
+
+    def initialize(attrs)
+      if attrs[:key] && attrs[:key].to_sym == :jshint
+        self.key  = :jshint
+        self.name = 'JSHint'
+      else
+        self.key  = :jslint
+        self.name = 'JSLint'
+      end
+
+      self.path = attrs[:path] || JSLintMate.lib_path("#{key}.js")
+
+      [:options, :options_filepath].each do |attr_key|
+        self.send(:"#{attr_key}=", attrs[attr_key])
+      end
+
+      self.options =
+        options ? JSLintMate::Linter.options_string_to_hash(options) : {}
+    end
+
+    def to_s; name; end
+
+    def options_string
+      JSLintMate::Linter.options_hash_to_string(options)
+    end
+
+    def reverse_merge_options_from_file!
+      return unless options_filepath && File.readable?(options_filepath)
+
+      require 'yaml'
+
+      # Parse linter options file; file options take precedence over existing
+      # options (i.e., default/custom bundle options)
+      file_options =
+        YAML.load_file(options_filepath).reject{ |k, v| v.is_a?(Array) }
+      self.options = options.merge(file_options)
+    end
+
+  end
+
+end
