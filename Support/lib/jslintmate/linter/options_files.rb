83dba839afd236d11028700ad329564dfdaeb8b5
Ron DeVera <hello@rondevera.com>
2012-01-04 00:04:03 -0800

    Add comments

--- Support/lib/jslintmate/linter/options_files.rb
+++ Support/lib/jslintmate/linter/options_files.rb
@@ -19,6 +19,10 @@ module JSLintMate
       end
 
       def read_options_from_yaml_file
+        # Sets `self.options_from_config_file` to a string representation of the
+        # options in `self.config_file_path`. Assumes that the file is readable
+        # and contains YAML.
+
         # Verify YAML syntax with `YAML.load_file`
         options_string = YAML.load_file(config_file_path)
 
@@ -28,6 +32,10 @@ module JSLintMate
       end
 
       def read_options_from_json_file
+        # Sets `self.options_from_config_file` to a string representation of the
+        # options in `self.config_file_path`. Assumes that the file is readable
+        # and contains JSON or evaluates to a JS object.
+
         # Convert JS file (containing valid JSON or JS) to a JSON string
         cmd = %{#{JSC_PATH} -e 'print(JSON.stringify(eval(arguments[0])))' } <<
               %{-- "($(cat #{config_file_path}))"}
